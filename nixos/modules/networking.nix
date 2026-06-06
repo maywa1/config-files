7784b655a81e85a12c99252c49b703c8ea38894b
maywai <maywai@proton.me>
2026-06-06 20:36:11 +0100

    BAM!

--- nixos/modules/networking.nix
+++ nixos/modules/networking.nix
@@ -2,8 +2,11 @@
 
 {
   networking.networkmanager.enable = true;
+
+  networking.networkmanager.dns = "none";
+
   networking.nameservers = [
-  "1.1.1.1"
-  "8.8.8.8"
-];
+    "1.1.1.1"
+    "8.8.8.8"
+  ];
 }
