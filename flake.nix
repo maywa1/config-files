a864db5cef6f9f1e15beb7c37df5e45b67e9f8ae
maywai <maywai@proton.me>
2026-06-07 00:13:06 +0100

    Added zen browser flake

--- flake.nix
+++ flake.nix
@@ -3,17 +3,28 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    zen-browser = {
+      url = "github:0xc000022070/zen-browser-flake";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = {
+  outputs = inputs@{
     self,
     nixpkgs,
+    ...
   }: let
     system = "x86_64-linux";
   in {
     nixosConfigurations = {
       nixie = nixpkgs.lib.nixosSystem {
-        specialArgs = {inherit system;};
+        inherit system;
+
+        specialArgs = {
+          inherit system inputs;
+        };
+
         modules = [
           ./nixos/configuration.nix
         ];
