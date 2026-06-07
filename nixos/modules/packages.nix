571f1c11d442c2b12acd6032e12ddd2e0c9aa63f
maywai <maywai@proton.me>
2026-06-07 18:40:03 +0100

    Automatic mounting of disks

--- nixos/modules/packages.nix
+++ nixos/modules/packages.nix
@@ -2,6 +2,7 @@
 
 {
   environment.systemPackages = with pkgs; [
+    udiskie
     hyprpaper
     tree-sitter
     brightnessctl
--- nixos/modules/services.nix
+++ nixos/modules/services.nix
@@ -1,5 +1,16 @@
-{ config, lib, pkgs, ... }:
+{ pkgs, ... }:
 
 {
   services.openssh.enable = true;
+
+  services.udisks2.enable = true;
+
+  systemd.user.services.udiskie = {
+    description = "Automount removable drives with udiskie";
+    wantedBy = [ "default.target" ];
+    serviceConfig = {
+      ExecStart = "${pkgs.udiskie}/bin/udiskie -t";
+      Restart = "on-failure";
+    };
+  };
 }
