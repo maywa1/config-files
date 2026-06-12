cd369e909b8e5b74656d5f31739846c449a59925
maywai <maywai@proton.me>
2026-06-12 23:49:08 +0100

    Changed the config to be specifically tailored to my laptop

--- nixos/modules/system.nix
+++ nixos/modules/system.nix
@@ -5,10 +5,19 @@
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  boot.kernelParams = [ "quiet" "splash" "loglevel=3" ];
   boot.plymouth.enable = false;
 
   boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.kernelParams = [
+    # Disable NVMe Autonomous Power State Transition (APST) to prevent the 31s SSD wake-up timeout
+    "nvme_core.default_ps_max_latency_us=0"
+    # Disable platform serial ports to prevent timeout on INT3515 phantom serial buses
+    "8250.nr_uarts=0"
+
+    "quiet"
+    "splash"
+    "loglevel=3"
+  ];
 
   networking.hostName = "nixie";
 
