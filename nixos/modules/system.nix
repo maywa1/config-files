e7a4032a497f9493d9a1dd19a3a9a4e9ad786c91
maywai <maywai@proton.me>
2026-06-14 21:48:20 +0100

    fixed wifi card not shutting down correctly (i think)

--- nixos/modules/system.nix
+++ nixos/modules/system.nix
@@ -11,8 +11,12 @@
   boot.kernelParams = [
     # Disable NVMe Autonomous Power State Transition (APST) to prevent the 31s SSD wake-up timeout
     "nvme_core.default_ps_max_latency_us=0"
+
     # Disable platform serial ports to prevent timeout on INT3515 phantom serial buses
     "8250.nr_uarts=0"
+
+    # Disable PCIe Active State Power Management to fix mt7921e Wi-Fi resume failure (error -110)
+    "pcie_aspm=off"
 
     "quiet"
     "splash"
