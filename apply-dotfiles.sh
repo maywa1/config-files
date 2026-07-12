42066fefe8c3d5fc563dba4cc270ab9748ec36f5
maywai <maywai@proton.me>
2026-07-12 17:49:35 +0100

    apply-dotfiles is also used to put the dotfiles in .config if they are not there

--- apply-dotfiles.sh
+++ apply-dotfiles.sh
@@ -4,15 +4,21 @@ set -euo pipefail
 DOTFILES="$HOME/nixos-conf/dotfiles"
 CONFIG="$HOME/.config"
 
+mkdir -p "$CONFIG"
+
 for dir in "$DOTFILES"/*; do
     [ -e "$dir" ] || continue
 
     name=$(basename "$dir")
+    source="$CONFIG/$name"
+    target="$DOTFILES/$name"
 
-    [ -e "$CONFIG/$name" ] || continue
-
-    rm -rf "$DOTFILES/$name"
-    cp -a "$CONFIG/$name" "$DOTFILES/$name"
-
-    echo "copied $name"
+    if [ -e "$source" ]; then
+        rm -rf "$target"
+        cp -a "$source" "$target"
+        echo "updated dotfiles: $name"
+    else
+        cp -a "$target" "$source"
+        echo "restored config: $name"
+    fi
 done
