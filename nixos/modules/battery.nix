581bd59759c1f48498b472df2f239df50d2ce80b
maywai <maywai@proton.me>
2026-07-14 12:48:47 +0100

    better battery settings, maybe it won't hardshutdown now...

--- nixos/modules/battery.nix
+++ nixos/modules/battery.nix
@@ -1,29 +1,39 @@
-{ config, ... }: {
-
-  powerManagement = {
-    enable = true;
-  };
+{
+  powerManagement.enable = true;
 
   services = {
-    upower = {
-      enable = true;
-      criticalPowerAction = "Hibernate";
-    };
+    logind.extraConfig = ''
+      HandleLowBattery=hibernate
+    '';
 
-    # Keep auto-cpufreq as your SINGLE source of truth for CPU power management
-    auto-cpufreq = {
+    upower.enable = true;
+
+    tlp = {
       enable = true;
       settings = {
-        battery = {
-          governor = "powersave";
-          turbo = "never";
-        };
-        charger = {
-          governor = "performance";
-          turbo = "auto";
-        };
+        CPU_SCALING_GOVERNOR_ON_AC = "performance";
+        CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+
+        CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+        CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+
+        PLATFORM_PROFILE_ON_AC = "performance";
+        PLATFORM_PROFILE_ON_BAT = "low-power";
+
+        PCIE_ASPM_ON_AC = "default";
+        PCIE_ASPM_ON_BAT = "powersupersave";
+
+        RUNTIME_PM_ON_AC = "auto";
+        RUNTIME_PM_ON_BAT = "auto";
+
+        USB_AUTOSUSPEND = 1;
+
+        WIFI_PWR_ON_AC = "off";
+        WIFI_PWR_ON_BAT = "on";
+
+        START_CHARGE_THRESH_BAT0 = 75;
+        STOP_CHARGE_THRESH_BAT0 = 80;
       };
     };
-
   };
 }
--- nixos/modules/system.nix
+++ nixos/modules/system.nix
@@ -10,13 +10,13 @@
   boot.kernelPackages = pkgs.linuxPackages_latest;
   boot.kernelParams = [
     # Disable NVMe Autonomous Power State Transition (APST) to prevent the 31s SSD wake-up timeout
-    "nvme_core.default_ps_max_latency_us=0"
+    # "nvme_core.default_ps_max_latency_us=0"
 
     # Disable platform serial ports to prevent timeout on INT3515 phantom serial buses
-    "8250.nr_uarts=0"
+    # "8250.nr_uarts=0"
 
     # Disable PCIe Active State Power Management to fix mt7921e Wi-Fi resume failure (error -110)
-    "pcie_aspm=off"
+    # "pcie_aspm=off"
 
     "quiet"
     "splash"
