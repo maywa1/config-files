326a331862224d17a37ee80022aec63862971d3d
maywai <maywai@proton.me>
2026-09-10 18:05:45 +0100

    darkmode

--- nixos/modules/env.nix
+++ nixos/modules/env.nix
@@ -1,16 +1,5 @@
 { ... }:
 {
-    environment.etc."xdg/qt5ct/qt5ct.conf".text = ''
-        [Appearance]
-        style=Fusion
-        color_scheme_path=/etc/xdg/qt5ct/colors/darker.conf
-        custom_palette=true
-    '';
-    environment.sessionVariables = {
-        GTK_THEME = "Adwaita:dark";
-        QT_QPA_PLATFORMTHEME = "qt5ct";
-        ELECTRON_FORCE_DARK_MODE = "1";
-    };
 
     environment.sessionVariables = {
         XCURSOR_THEME = "Bibata-Modern-Classic";
