2600dfc3bc86fab9b063b90ba1625fc4832f770c
maywai <maywai@proton.me>
2026-09-10 18:34:20 +0100

    slock permissions fix

--- nixos/modules/programs.nix
+++ nixos/modules/programs.nix
@@ -1,6 +1,14 @@
-{  ... }:
+{pkgs,  ... }:
 
 {
     programs.dconf.enable = true;
     programs.git.enable = true;
+
+    security.wrappers.slock = {
+        source = "${pkgs.slock}/bin/slock";
+        owner = "root";
+        group = "root";
+        setuid = true;
+    };
+
 }
